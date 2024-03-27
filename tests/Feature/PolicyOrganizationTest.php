800b0fc1b71b339a7adf521d9ec311d40ebda2f7
domi <dburea01@gmail.com>
2024-03-27 16:35:31 +0100

    tests

--- tests/Feature/PolicyOrganizationTest.php
+++ tests/Feature/PolicyOrganizationTest.php
@@ -25,17 +25,9 @@ class PolicyOrganizationTest extends TestCase
         $organization = Organization::factory()->create();
         $url = $this->getEndPoint().self::RESOURCE;
 
-        $superAdmin = User::factory()->create([
-            'role_id' => 'SUPERADMIN'
-        ]);
-        $admin = User::factory()->create([
-            'role_id' => 'ADMIN',
-            'organization_id' => $organization->id
-        ]);
-        $user = User::factory()->create([
-            'role_id' => 'USER',
-            'organization_id' => $organization->id
-        ]);
+        $superAdmin = $this->createUser('SUPERADMIN', null);
+        $admin = $this->createUser('ADMIN', $organization);
+        $user = $this->createUser('USER', $organization);
 
         $this->actingAs($superAdmin)->getJson($url)->assertOk();
         $this->actingAs($admin)->getJson($url)->assertForbidden();
@@ -47,17 +39,9 @@ class PolicyOrganizationTest extends TestCase
         $organization = Organization::factory()->create();
         $url = $this->getEndPoint().self::RESOURCE;
 
-        $superAdmin = User::factory()->create([
-            'role_id' => 'SUPERADMIN'
-        ]);
-        $admin = User::factory()->create([
-            'role_id' => 'ADMIN',
-            'organization_id' => $organization->id
-        ]);
-        $user = User::factory()->create([
-            'role_id' => 'USER',
-            'organization_id' => $organization->id
-        ]);
+        $superAdmin = $this->createUser('SUPERADMIN', null);
+        $admin = $this->createUser('ADMIN', $organization);
+        $user = $this->createUser('USER', $organization);
 
         $this->actingAs($superAdmin)->postJson($url)->assertUnprocessable();
         $this->actingAs($admin)->postJson($url)->assertForbidden();
@@ -69,17 +53,9 @@ class PolicyOrganizationTest extends TestCase
         $organization = Organization::factory()->create();
         $url = $this->getEndPoint().self::RESOURCE.'/'.$organization->id;
 
-        $superAdmin = User::factory()->create([
-            'role_id' => 'SUPERADMIN'
-        ]);
-        $admin = User::factory()->create([
-            'role_id' => 'ADMIN',
-            'organization_id' => $organization->id
-        ]);
-        $user = User::factory()->create([
-            'role_id' => 'USER',
-            'organization_id' => $organization->id
-        ]);
+        $superAdmin = $this->createUser('SUPERADMIN', null);
+        $admin = $this->createUser('ADMIN', $organization);
+        $user = $this->createUser('USER', $organization);
 
         $this->actingAs($superAdmin)->putJson($url)->assertUnprocessable();
         $this->actingAs($admin)->putJson($url)->assertForbidden();
@@ -91,17 +67,9 @@ class PolicyOrganizationTest extends TestCase
         $organization = Organization::factory()->create();
         $url = $this->getEndPoint().self::RESOURCE.'/'.$organization->id;
 
-        $superAdmin = User::factory()->create([
-            'role_id' => 'SUPERADMIN'
-        ]);
-        $admin = User::factory()->create([
-            'role_id' => 'ADMIN',
-            'organization_id' => $organization->id
-        ]);
-        $user = User::factory()->create([
-            'role_id' => 'USER',
-            'organization_id' => $organization->id
-        ]);
+        $superAdmin = $this->createUser('SUPERADMIN', null);
+        $admin = $this->createUser('ADMIN', $organization);
+        $user = $this->createUser('USER', $organization);
 
         $this->actingAs($superAdmin)->getJson($url)->assertOK();
         $this->actingAs($admin)->getJson($url)->assertOK();
@@ -113,21 +81,21 @@ class PolicyOrganizationTest extends TestCase
         $organization = Organization::factory()->create();
         $url = $this->getEndPoint().self::RESOURCE.'/'.$organization->id;
 
-        $superAdmin = User::factory()->create([
-            'role_id' => 'SUPERADMIN'
-        ]);
-        $admin = User::factory()->create([
-            'role_id' => 'ADMIN',
-            'organization_id' => $organization->id
-        ]);
-        $user = User::factory()->create([
-            'role_id' => 'USER',
-            'organization_id' => $organization->id
-        ]);
-
+        $superAdmin = $this->createUser('SUPERADMIN', null);
+        $admin = $this->createUser('ADMIN', $organization);
+        $user = $this->createUser('USER', $organization);
+       
         $this->actingAs($admin)->deleteJson($url)->assertForbidden();
         $this->actingAs($user)->deleteJson($url)->assertForbidden();
-        $this->actingAs($superAdmin)->deleteJson($url)->assertNoContent();
-        
+        $this->actingAs($superAdmin)->deleteJson($url)->assertNoContent(); 
+    }
+
+    public function createUser(string $roleId, ?Organization $organization) : User
+    {
+        return User::factory()->create([
+            'role_id' => 'SUPERADMIN',
+            'organization_id' => $organization ? $organization->id : null,
+            'role_id' => $roleId
+        ]);
     }
 }
